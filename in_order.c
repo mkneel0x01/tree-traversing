b1efbb86d82bbaa0206e7617e44a085887089a6f
mkneel0x01 <mkneel101@gmail.com>
2024-05-30 22:12:35 +0600

    complete pre,post & in-order

--- in_order.c
+++ in_order.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// Definition of the tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new node
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+// In-order traversal function
+void inOrderTraversal(struct Node* node) {
+    if (node == NULL)
+        return;
+
+    // Recursively traverse the left subtree
+    inOrderTraversal(node->left);
+
+    // Print the data of the node
+    printf("%d ", node->data);
+
+    // Recursively traverse the right subtree
+    inOrderTraversal(node->right);
+}
+
+// Example usage
+int main() {
+    // Creating a simple tree
+    //        1
+    //       / \
+    //      2   3
+    //     / \
+    //    4   5
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    printf("In-order traversal of the binary tree is:\n");
+    inOrderTraversal(root);
+
+    return 0;
+}
--- post_order.c
+++ post_order.c
@@ -1,1 +1,54 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of the tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new node
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+// Post-order traversal function
+void postOrderTraversal(struct Node* node) {
+    if (node == NULL)
+        return;
+
+    // Recursively traverse the left subtree
+    postOrderTraversal(node->left);
+
+    // Recursively traverse the right subtree
+    postOrderTraversal(node->right);
+
+    // Print the data of the node
+    printf("%d ", node->data);
+}
+
+// Example usage
+int main() {
+    // Creating a simple tree
+    //        1
+    //       / \
+    //      2   3
+    //     / \
+    //    4   5
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    printf("Post-order traversal of the binary tree is:\n");
+    postOrderTraversal(root);
+
+    return 0;
+}
